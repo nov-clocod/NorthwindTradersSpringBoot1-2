6dbb2a19bcd897a46435bdd0a4e8f5c645a63be1
Sthealth <AW@example.com>
2025-12-04 16:45:48 -0500

    add code to ProductService layer

--- src/main/java/com/pluralsight/NorthwindTradersSpringBoot/services/ProductService.java
+++ src/main/java/com/pluralsight/NorthwindTradersSpringBoot/services/ProductService.java
@@ -1,4 +1,36 @@
 package com.pluralsight.NorthwindTradersSpringBoot.services;
 
+import com.pluralsight.NorthwindTradersSpringBoot.dao.interfaces.IProductDAO;
+import com.pluralsight.NorthwindTradersSpringBoot.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class ProductService {
+    private IProductDAO productDAO;
+
+    @Autowired
+    public ProductService(IProductDAO productDAO) {
+        this.productDAO = productDAO;
+    }
+
+    public Product addProduct(Product product) {
+        return productDAO.addProduct(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return productDAO.getAllProducts();
+    }
+
+    public Product getProductById(int productId) {
+        return productDAO.getProductByID(productId);
+    }
+
+    public void updateProduct(int productId, Product product) {
+        productDAO.update(productId, product);
+    }
+
+    public void deleteProduct(int productId) {
+        productDAO.delete(productId);
+    }
 }
